609bf02762e836058376bd829d32caeb771e235f
camsmith293 <camsmith293@gmail.com>
2016-03-30 23:39:03 -0400

    Added method to cluster digits using EM

--- src/HighCommand.py
+++ src/HighCommand.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_digits, load_iris
 
 from kMeansClusterer import kMeansClusterer
+from EMClusterer import EMClusterer
 
 digits = load_digits()
 iris = load_iris()
@@ -11,6 +12,11 @@ iris_name = "Iris Dataset"
 def kMeansClusterDigits():
     kMeans = kMeansClusterer(digits, 10, digits_name)
     kMeans.cluster()
-    kMeans.display_centroids()
+    kMeans.display_digits_centroids()
+
+def EMClusterDigits():
+    EM = EMClusterer(digits, 10, digits_name)
+    EM.cluster()
+    EM.display_digits_centroids()
 
 kMeansClusterDigits()
