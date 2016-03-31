28b4251740259389ad209c33ff4f4eb8dd2c00ee
camsmith293 <camsmith293@gmail.com>
2016-03-30 22:59:43 -0400

    Added Expected Maximum Clusterer

---
+++ src/EMClusterer.py
@@ -0,0 +1,45 @@
+from matplotlib import cm
+from sklearn.mixture import GMM
+from sklearn.preprocessing import scale
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class EMClusterer():
+
+    def __init__(self, dataset, num_clusters, dataset_name):
+        self.dataset = dataset
+        self.num_clusters = num_clusters
+        self.dataset_name = dataset_name
+
+        self.clusterers = dict((covar_type, GMM(n_components=num_clusters,
+                    covariance_type=covar_type, init_params='wc', n_iter=500))
+                   for covar_type in ['spherical', 'diag', 'tied', 'full'])
+
+    def cluster(self, iterations=500):
+        for index, (name, classifier) in enumerate(self.clusterers.items()):
+
+
+    def display_digits_centroids(self):
+        plt.figure(figsize=(4.2, 4))
+        # for clusterer in self.clusterers:
+        #     for i, patch in enumerate(clusterer.cluster_centers_):
+        #         print(patch, len(patch))
+        #         plt.subplot(10, 10, i + 1)
+        #         plt.imshow(patch.reshape(8, 8), cmap=plt.cm.gray,
+        #                    interpolation='nearest')
+        #         plt.xticks(())
+        #         plt.yticks(())
+
+        colors = cm.rainbow(np.linspace(0, 1, 10))
+
+        for n, color in enumerate(colors):
+            data = self.dataset.data[self.dataset.target == n]
+            plt.scatter(data[:, 0], data[:, 1], 0.8, color=color,
+                        label=self.dataset.target_names[n])
+
+
+        plt.suptitle('Centroids of Minibatch KMeans Clustering of\n ' + self.dataset_name)
+        plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+
+        plt.show()
