3b2ef6d79ddf05bd53742f84a6c0261a93937887
camsmith293 <camsmith293@gmail.com>
2016-04-01 10:31:37 -0400

    Added Nonnegative Matrix Factorization dimensionality reducer

---
+++ src/DimensionalityReduction/NMFReducer.py
@@ -0,0 +1,15 @@
+from sklearn.decomposition import NMF
+
+class ICAReducer():
+
+    def __init__(self, dataset, dataset_name):
+        self.dataset = dataset
+        self.dataset_name = dataset_name
+
+        self.data = dataset.data
+        self.n_components = len(self.data[0])
+
+        self.reducer = NMF(n_components=self.n_components)
+
+    def __reduce__(self):
+        return self.reducer.fit_transform(self.data)
