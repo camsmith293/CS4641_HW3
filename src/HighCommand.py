aeecca3f0d965f396815e7961469d675dfaeb44f
camsmith293 <camsmith293@gmail.com>
2016-04-01 12:56:53 -0400

    Added method to reduce digits using all 4 algorithms

--- src/HighCommand.py
+++ src/HighCommand.py
@@ -2,6 +2,10 @@ from sklearn.datasets import load_digits, load_iris
 
 from kMeansClusterer import kMeansClusterer
 from EMClusterer import EMClusterer
+from DimensionalityReduction.PCAReducer import PCAReducer
+from DimensionalityReduction.ICAReducer import ICAReducer
+from DimensionalityReduction.RCAReducer import RCAReducer
+from DimensionalityReduction.NMFReducer import NMFReducer
 
 digits = load_digits()
 iris = load_iris()
@@ -28,5 +32,15 @@ def EMClusterIris():
     EM = EMClusterer(iris, 3, iris_name)
     EM.cluster()
     EM.display_iris_clusterings()
+
+def reduce_digits:
+    reducers = [PCAReducer(digits, digits_name),
+                ICAReducer(digits, digits_name),
+                RCAReducer(digits, digits_name),
+                NMFReducer(digits, digits_name)]
+
+    for reducer in reducers:
+        reducer.reduce()
+        reducer.display_reduced_digits()
 
 kMeansClusterIris()
