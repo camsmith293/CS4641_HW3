942a6ab2bd3e01aeda80ee4462be9a8f8aad1d66
camsmith293 <camsmith293@gmail.com>
2016-04-04 18:09:19 -0400

    Output for reduction then clustering

--- src/kMeansClusterer.py
+++ src/kMeansClusterer.py
@@ -1,6 +1,8 @@
+import sys
+
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import scale
+from sklearn.preprocessing import MinMaxScaler
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,17 +13,21 @@ class kMeansClusterer():
         self.dataset = dataset
         self.num_clusters = num_clusters
         self.dataset_name = dataset_name
+        self.scaler = MinMaxScaler()
+        self.data = self.scaler.fit_transform(self.dataset.data)
 
-        self.clusterer = KMeans(self.num_clusters, verbose=1)
+        self.clusterer = KMeans(n_clusters=self.num_clusters, verbose=1)
 
-    def cluster(self, iterations=500, data = None):
+    def cluster(self, iterations=500):
         self.clusterer.max_iter = iterations
-        #data = scale(self.dataset.data)
-        if data is None:
-            data = self.dataset.data
-        self.clusterer.fit(data)
+        self.clusterer.fit(self.data)
 
-    def display_digits_centroids(self):
+    def reduce_data(self, reducer):
+        reduced = reducer.reduce()
+        self.data = self.scaler.fit_transform(reduced)
+        # self.cluster()
+
+    def display_digits_centroids(self, outfile='kMeansDigitsClusterings.png'):
         plt.figure(figsize=(4.2, 4))
         for i, patch in enumerate(self.clusterer.cluster_centers_):
             plt.subplot(10, 10, i + 1)
@@ -35,15 +41,16 @@ class kMeansClusterer():
         plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
 
         plt.show()
+        plt.savefig(outfile)
 
-    def display_iris_clusterings(self):
+    def display_iris_clusterings(self, outfile='kMeansIrisClusterings.png'):
         fig = plt.figure(figsize=(4, 3))
         plt.clf()
         ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
 
         plt.cla()
 
-        X = self.dataset.data
+        X = self.data
         labels = self.clusterer.labels_
 
         ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=labels.astype(np.float))
@@ -56,3 +63,14 @@ class kMeansClusterer():
         ax.set_zlabel('Petal length')
 
         plt.show()
+        plt.savefig(outfile)
+
+    def display_reduced_clusterings(self, reducer):
+        filename = "kMeans" + self.dataset_name + type(reducer).__name__ + "reduction.txt"
+        sys.stdout(filename)
+        print("%s Reduction and Clustering" % type(reducer).__name__)
+        print(40 * '-')
+        reducer.benchmark(self, "Pre-Reduction", self.data)
+        self.reduce_data(reducer)
+        reducer.benchmark(self, "Post-Reduction", self.data)
+
